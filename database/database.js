347f7c29792dedcfc266bfa7aa0581fd78bd1171
vinayak1315 <59113859+vinayak1315@users.noreply.github.com>
2023-04-14 03:42:51 +0530

    db connection

--- database/database.js
+++ database/database.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-exports.connection = mongoose.connect(process.env.localUri, { useNewUrlParser: true }).then(con => {
+exports.connection = mongoose.connect(process.env.localUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(con => {
     console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
 })
